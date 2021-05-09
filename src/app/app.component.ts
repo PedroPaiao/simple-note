3d632e3a18eea6b9f79ca54917fb458247dd6c23
pedro <pedro.silva@pontomais.com.br>
2021-05-09 19:26:37 -0300

    [UPDATE] Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,67 +11,11 @@ import { NewTaskModalComponent } from './components/new-task-modal/new-task-moda
 })
 export class AppComponent implements OnInit {
   title = 'simple-note-frontend';
-  logged = true;
-  closeResult = "";
-  value = ""
-  showFiller = true;
-  openDrawer = false;
-
-  optionsDrawer = [
-    {view: 'Todas as tarefas', link: '' },
-    {view: 'Tarefas de hoje', link: 'today' },
-    {view: 'Completas', link: 'done' },
-    {view: 'Atrasadas', link: 'late' },
-    {view: 'Sem data marcada', link: 'unscheduled'},
-    {view: 'Importantes', link: 'important' },
-    {view: 'Calendario', link: 'calendar' },
-  ]
 
   constructor(
     public dialog: MatDialog,
     private _route: Router 
   ) {}
 
-  ngOnInit(): void {
-    console.log(this._route.url)
-    if(this._route.url == '/') {
-      this.logged = false
-    } else {
-      this.logged = true
-    }
-    // this._route.events.filter()
-    // console.log()
-      // if(event == 'RoutesRecognized' ) {}    // this.logged = Login.getLogin();
-  }
-
-  doLogin() {
-    // console.log("banana 2")
-    // this.logged = true;
-  }
-
-  openDialog() {
-    const dialogRef = this.dialog.open(NewTaskModalComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
-
-  changeDrawer() {
-    this.openDrawer = !this.openDrawer;
-  }
-
-  reditectToPage(option: string) {
-    // console.log(option)
-    let currentUrl = this._route.url;
-    this._route.routeReuseStrategy.shouldReuseRoute = () => false;
-    this._route.onSameUrlNavigation = 'reload';
-    // this._route.navigate([currentUrl]);
-    this._route.navigate(['/list'], {
-      queryParams: {
-        list: option
-      }
-    })
-    console.log(this._route.url)
-  }
+  ngOnInit(): void {}
 }
